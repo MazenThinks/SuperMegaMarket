120b46285d1619979f40ac4a2af34cbcafb71795
MazenThinks <mazenmagdy100.mm@gmail.com>
2023-06-23 17:12:52 +0300

    delete products

--- crud/delete.php
+++ crud/delete.php
@@ -3,10 +3,10 @@
 
     // if the 'id' variable is set in the URL, we hnow that we need to edit a record
     if(isset($_GET['id'])){
-        $user_id = $_GET['id'];
+        $product_id = $_GET['id'];
 
         //write delete query
-        $sql  = "DELETE FROM `users` WHERE `id`= '$user_id'";
+        $sql  = "DELETE FROM `products` WHERE `id`= '$product_id'";
 
         //execute the query
         $result = $conn->query($sql);
